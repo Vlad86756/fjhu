fb326639af6edb1df23cb360541e879a2e1155a1
Vlad86756 <145270130+Vlad86756@users.noreply.github.com>
2023-09-24 17:16:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,29 +3,55 @@ from random import randint
 
 app = QApplication([])
 win = QWidget()
-win.setWindowTitle("Визначник переможця")
-win.resize(400, 200)
+win.setWindowTitle("Лотерея")
+win.resize(400, 400)
 win.move(100, 100)
 
 text=QLabel(win)
-text.setText("Натисніть, щоб дізнатись переможця")
+text.setText("Натисни, щоб взяти участь")
 text.move(70, 10)
 
 text2 = QLabel(win)
 text2.setText("?")
 text2.move(190, 70)
 
+text3 = QLabel(win)
+text3.setText("?")
+text3.move(190, 100)
+
+
+
+
+
+
+
+
+
+
 button = QPushButton(win)
-button.setText("Згенерувати")
+button.setText("Випробувати удачу")
 button.move(140, 130)
+
+
+
+
+
+
+
 
 def show_win():
     number = randint(1, 100)
+    number1= randint(1, 100)
     text2.setText(str(number))
-    text.setText("Переможець:")
+
+    text3.setText(str(number1))
+
+    if number == number1:
+        text.setText("Ви виграли! Зіграйте знову")
+    elif number != number1:
+        text.setText("Ви програли! Зіграйте знову")
+
 button.clicked.connect(show_win)
-
-
 
 
 
